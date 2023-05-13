37318ed4036c30e62c50d324435a935b24a760f0
PK <nozumaz@gmail.com>
2023-05-13 05:04:45 -0400

    testing fetching into pokemon object

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const fetchPkmn = () => {
   })
   .then((data) => {
     console.log(data);
-    const pokemon = {};
-    pokemon['name'] = data.name;
-    pokemon['id'] = data.id;
-    pokemon['image'] = data.sprites['front_default'];
-    pokemon['type'] = data.types
-      .map((type) => type.type.name)
-      .join(', ');
+    const pokemon = {
+      name: data.name,
+      id: data.id,
+      image: data.sprites['front_default'],
+      type: data.types.map((type) => type.type.name).join (', ')
+    };
     console.log(pokemon);
   })
 }
