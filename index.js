764dae41e0c19a8dd7a80670dfc8785c46e37cfe
PK <nozumaz@gmail.com>
2023-05-13 05:43:21 -0400

    testing fetching with promises

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const fetchPkmn = () => {
-  for (let i = 1; i <= 150; i++) {
+  const promises = [];
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url).then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const pokemon = {
-        name: data.name,
-        id: data.id,
-        image: data.sprites['front_default'],
-        type: data.types.map((type) => type.type.name).join (', ')
-      };
-      console.log(pokemon);
-    })      
+    promises.push(fetch(url).then((res) => res.json()));
   }
+
+  Promise.all(promises).then(results => {
+    const pokemon = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites['front_default'],
+      type: data.types.map((type) => type.type.name).join (', ')
+    }))
+    console.log(pokemon);
+  })
 }
+
 
 fetchPkmn();
